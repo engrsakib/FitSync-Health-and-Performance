2e3671cc6e1f5e34fdfd375d831ef53a6fbd15bf
engrsakib <snazmussakib01@gmail.com>
2025-08-25 17:19:38 +0600

    authorization update

--- server/src/app/modules/scheduling/scheduling.routes.ts
+++ server/src/app/modules/scheduling/scheduling.routes.ts
@@ -8,8 +8,8 @@ import { SchedulingController } from "./scheduling.controller";
 
 const router = Router();
 router.post("/create",verifyToken(role.ADMIN),validateRequest(createScheduleZodSchema), SchedulingController.createScheduling);
-router.get("/", SchedulingController.getAllSchedulings);
-router.get("/:slug", SchedulingController.getSingleScheduling);
+router.get("/", verifyToken(role.ADMIN, role.TRAINEE, role.TRAINER), SchedulingController.getAllSchedulings);
+router.get("/:slug", verifyToken(role.ADMIN, role.TRAINEE, role.TRAINER), SchedulingController.getSingleScheduling);
 router.patch("/:id", verifyToken(role.ADMIN), validateRequest(updateScheduleZodSchema), SchedulingController.updateScheduling);
 router.delete("/:id", verifyToken(role.ADMIN), SchedulingController.deleteScheduling);
 
